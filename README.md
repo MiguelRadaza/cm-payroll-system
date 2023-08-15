d8d1d2a27dfb58cd106366cbdeeb36f0c24096e8
Miguel Igdalino <miguel.igdalino@growsari.com>
2023-08-16 00:37:05 +0800

    added component widget to boot

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ##Connect to WebServer and do composer install
     docker exec -it {docker sail id} "/bin/bash"
+    cd domains/chomuntinlupa.com/public_html/cm-payroll/src
+
 
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component('smallBoxWidget', \App\View\Components\SmallBoxWidget::class);
+        Blade::component('alert', \App\View\Components\SmallBoxWidget::class);
+        Blade::component('alertWidget', \App\View\Components\SmallBoxWidget::class);
     }
 }
