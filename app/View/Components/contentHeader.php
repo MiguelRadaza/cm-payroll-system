da804572fba099a580e9ee224c8ec2aa1a765cdc
Miguel Igdalino <miguel.igdalino@growsari.com>
2023-08-16 01:07:28 +0800

    added temp fix

--- app/View/Components/contentHeader.php
+++ app/View/Components/contentHeader.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class contentHeader extends Component
+class ContentHeader extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/smallBoxWidget.php
+++ app/View/Components/smallBoxWidget.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class smallBoxWidget extends Component
+class SmallBoxWidget extends Component
 {
     /**
      * Create a new component instance.
