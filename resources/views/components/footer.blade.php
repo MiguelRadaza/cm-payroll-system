a8374680d68bd97077d668fc18ded6bff70bac15
Miguel Igdalino <miguel.igdalino@growsari.com>
2023-08-18 00:32:58 +0800

    added fix on footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,7 @@
     <!--begin::Copyright-->
     <strong>
         Copyright &copy; 2023
-        <a href="https://adminlte.io">Miguel Radaza</a>.
+        <a href="https://cm-payroll.chomuntinlupa.com">CM Payroll</a>.
     </strong>
     All rights reserved.
     <!--end::Copyright-->
